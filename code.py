49645baee5a74aaedab0e8acd794b5b4d267ac14
qsawasd <s1083883@student.knoxschools.org>
2025-10-15 14:28:35 -0400

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,27 @@
+## Imports and basic variables
+import time
+import math
+
+## user data and core functions
+user_data = {
+    "Guest": {
+        "password": "Password",
+        "permrank": "Guest"
+    },
+    "Wesley": {
+        "password": "placeholder",
+        "permrank": "Operator"
+    }
+}
+
+def Login():
+    print("Dumb .PY OS\nAll inputs are case sensitive!")
+    while True:
+        user = input("Username: ")
+        passcheck = input("Password: ")
+        if user in user_data and passcheck == user_data[user]["password"]:
+            print("Login successful!")
+            break
+        else:
+            print("Invalid username or password. Please try again.\n")
 
